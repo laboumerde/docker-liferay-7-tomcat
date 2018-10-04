8d0c9931f0460aebd986f8b73f8d77ea85f36f43
Julien Boumard <julien.boumard@savoirfairelinux.com>
2018-10-03 23:10:14 -0400

    Change tomcat CATALINA_OPTS

---
+++ liferay-7-tomcat/assets/conf/liferay/tomcat/bin/setenv.sh
@@ -0,0 +1,7 @@
+CATALINA_OPTS="$CATALINA_OPTS -Dfile.encoding=UTF8 -Djava.net.preferIPv4Stack=true -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false -Duser.timezone=GMT -Xmx4096m -XX:MaxMetaspaceSize=512m"
+
+JMX_OPTS="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.port=8099 -Dcom.sun.management.jmxremote.ssl=false"
+
+CATALINA_OPTS="${CATALINA_OPTS} ${JMX_OPTS}"
+
+JPDA_ADDRESS="8000"
