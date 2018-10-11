fc82a2d274bf66da1b2cde79d4aa9918e536bbb7
Julien Boumard <julien.boumard@savoirfairelinux.com>
2018-10-10 21:34:25 -0400

    Reduced max allocated memory

--- liferay-7-tomcat/assets/conf/liferay/tomcat/bin/setenv.sh
+++ liferay-7-tomcat/assets/conf/liferay/tomcat/bin/setenv.sh
@@ -1,4 +1,4 @@
-CATALINA_OPTS="$CATALINA_OPTS -Dfile.encoding=UTF8 -Djava.net.preferIPv4Stack=true -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false -Duser.timezone=GMT -Xmx4096m -XX:MaxMetaspaceSize=512m"
+CATALINA_OPTS="$CATALINA_OPTS -Dfile.encoding=UTF8 -Djava.net.preferIPv4Stack=true -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false -Duser.timezone=GMT -Xmx3072m -XX:MaxMetaspaceSize=512m"
 
 JMX_OPTS="-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.port=8099 -Dcom.sun.management.jmxremote.ssl=false"
 
